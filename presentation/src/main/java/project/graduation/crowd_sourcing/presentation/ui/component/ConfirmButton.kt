3afb7ee84730aafb51e98a77f76992e082e0e86e
yang dong won <didehddnjs89@gmail.com>
2025-03-31 15:45:49 +0900

    feat, refactory/#2: common button, listitem 작성

---
+++ presentation/src/main/java/project/graduation/crowd_sourcing/presentation/ui/component/ConfirmButton.kt
@@ -0,0 +1,34 @@
+package project.graduation.crowd_sourcing.presentation.ui.component
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.Button
+import androidx.compose.material3.ButtonDefaults
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.colorResource
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import project.graduation.crowd_sourcing.presentation.R
+
+@Composable
+fun ConfirmButton(
+    text: String,
+    onConfirm: () -> Unit
+) {
+    Button(
+        onClick = { onConfirm() },
+        colors = ButtonDefaults.buttonColors(containerColor = colorResource(R.color.primary)),
+        shape = RoundedCornerShape(6.dp)
+    ) {
+        Text(text, color = Color.White)
+    }
+}
+
+@Preview
+@Composable
+fun ConfirmButtonPrev(){
+    ConfirmButton(
+        "apply"
+    ) { }
+}
