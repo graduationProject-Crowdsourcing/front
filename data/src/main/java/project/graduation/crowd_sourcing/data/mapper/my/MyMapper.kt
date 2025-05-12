ba8bea32a8b8b03e84fa121b714e3d35f8fed2f7
yang dong won <didehddnjs89@gmail.com>
2025-05-12 13:34:34 +0900

    refactory/#25: mapper에 stringToRegion 추가

--- data/src/main/java/project/graduation/crowd_sourcing/data/mapper/my/MyMapper.kt
+++ data/src/main/java/project/graduation/crowd_sourcing/data/mapper/my/MyMapper.kt
@@ -3,9 +3,9 @@ package project.graduation.crowd_sourcing.data.mapper.my
 import android.os.Build
 import androidx.annotation.RequiresApi
 import project.graduation.crowd_sourcing.data.mapper.parseIso8601ToDateSafe
+import project.graduation.crowd_sourcing.data.mapper.stringToRegion
 import project.graduation.crowd_sourcing.data.response.my.RecentCommissionResponse
 import project.graduation.crowd_sourcing.data.response.my.RecentWorkResponse
-import project.graduation.crowd_sourcing.domain.model.Region
 import project.graduation.crowd_sourcing.domain.model.entity.my.RecentCommissionEntity
 import project.graduation.crowd_sourcing.domain.model.entity.my.RecentWorkEntity
 import java.util.Date
@@ -14,7 +14,7 @@ import java.util.Date
 fun RecentCommissionResponse.toEntity(): RecentCommissionEntity = RecentCommissionEntity(
     id = this.id,
     commission = this.commission,
-    region = Region.from(this.region),
+    region = stringToRegion(this.region),
     category = this.category,
     commissionDate = parseIso8601ToDateSafe(this.commissionDate) ?: Date(),
     commissionPoint = this.commissionPoint
@@ -24,7 +24,7 @@ fun RecentCommissionResponse.toEntity(): RecentCommissionEntity = RecentCommissi
 fun RecentWorkResponse.toEntity(): RecentWorkEntity = RecentWorkEntity(
     id = this.id,
     work = this.work,
-    region = Region.from(this.region),
+    region = stringToRegion(this.region),
     category = this.category,
     item = this.item,
     itemPrice = this.itemPrice,
--- data/src/main/java/project/graduation/crowd_sourcing/data/mapper/userpoint/UserPointMapper.kt
+++ data/src/main/java/project/graduation/crowd_sourcing/data/mapper/userpoint/UserPointMapper.kt
@@ -3,6 +3,7 @@ package project.graduation.crowd_sourcing.data.mapper.userpoint
 import android.os.Build
 import androidx.annotation.RequiresApi
 import project.graduation.crowd_sourcing.data.mapper.parseIso8601ToDateSafe
+import project.graduation.crowd_sourcing.data.mapper.stringToRegion
 import project.graduation.crowd_sourcing.data.response.userpoint.UserPointHistoryResponse
 import project.graduation.crowd_sourcing.domain.model.Region
 import project.graduation.crowd_sourcing.domain.model.entity.userpoint.UserPointHistoryEntity
@@ -11,7 +12,7 @@ import java.util.Date
 @RequiresApi(Build.VERSION_CODES.O)
 fun UserPointHistoryResponse.toEntity(): UserPointHistoryEntity = UserPointHistoryEntity(
     type = this.type,
-    region = Region.from(this.region),
+    region = stringToRegion(this.region),
     item = this.item,
     point = this.point,
     date = parseIso8601ToDateSafe(this.date) ?: Date()
