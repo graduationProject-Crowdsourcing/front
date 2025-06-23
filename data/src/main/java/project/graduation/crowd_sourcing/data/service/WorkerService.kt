7b18fa8b63a4692a459ad7c5af4e7396a40d7329
yang dong won <didehddnjs89@gmail.com>
2025-06-23 16:38:19 +0900

    feat/#

--- data/src/main/java/project/graduation/crowd_sourcing/data/service/WorkerService.kt
+++ data/src/main/java/project/graduation/crowd_sourcing/data/service/WorkerService.kt
@@ -1,14 +1,17 @@
 package project.graduation.crowd_sourcing.data.service
 
+import project.graduation.crowd_sourcing.data.request.worker.PostAssignmentRequest
 import project.graduation.crowd_sourcing.data.request.worker.PostWorkRequest
 import project.graduation.crowd_sourcing.data.response.worker.WorkCountResponse
 import project.graduation.crowd_sourcing.data.response.worker.WorkHistoryResponse
 import project.graduation.crowd_sourcing.data.response.worker.WorkHourResponse
 import project.graduation.crowd_sourcing.data.response.worker.WorkMostResponse
 import project.graduation.crowd_sourcing.data.response.worker.WorkPointResponse
+import retrofit2.Response
 import retrofit2.http.Body
 import retrofit2.http.GET
 import retrofit2.http.POST
+import retrofit2.http.Path
 import retrofit2.http.Query
 
 
@@ -17,6 +20,14 @@ interface WorkerService {
     suspend fun postWorker(
         @Body request: PostWorkRequest
     ): Int
+
+    @POST("/api/v1/worker/assignment/{assignmentId}/submit")
+    suspend fun postAssignment(
+        @Body request: PostAssignmentRequest,
+        @Path("assignmentId") assignmentId: Int,
+        @Query("username") username: String
+    ): Response<Unit>
+
 
     @GET("/api/v1/worker/workstats")
     suspend fun getWorkerCounts(
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     compileSdk = 35
 
     defaultConfig {
-        minSdk = 25
+        minSdk = 26
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 
