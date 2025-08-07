2a0033012b29a258141a8ac7df04bff0cc519617
yang dong won <didehddnjs89@gmail.com>
2025-08-07 15:08:17 +0900

    feat/#41: 의뢰기록 중복 해결

--- presentation/src/main/java/project/graduation/crowd_sourcing/presentation/ui/screen/history/HistoryViewModel.kt
+++ presentation/src/main/java/project/graduation/crowd_sourcing/presentation/ui/screen/history/HistoryViewModel.kt
@@ -23,91 +23,91 @@ class HistoryViewModel @Inject constructor(
 
 
     fun loadHistoryData(historyType: HistoryType) = viewModelScope.launch {
-            when (historyType) {
-                HistoryType.WORK -> {
-                    historyUseCase.getCommission().onSuccess { data ->
-                        _uiState.update {
-                            HistoryUiState(
-                                stats = StatsType.Work.All(
-                                    totalWork = data.completed,
-                                    totalTime = data.countOrHour,
-                                    totalPoint = data.point
-                                ) to StatsType.Work.Detail(
-                                    mostRegion = data.mostRegion.koreanName,
-                                    averageTime = data.countOrHour / data.completed,
-                                    mostCategory = (data.currentList + data.completedList)
-                                        .map { it.category }
-                                        .groupingBy { it }
-                                        .eachCount()
-                                        .maxByOrNull { it.value }
-                                        ?.key ?: ""
-                                ),
-                                currentHistoryList = data.currentList.map {
-                                    HistoryItem(
-                                        product = it.commission,
-                                        category = it.category,
-                                        date = it.commissionDate,
-                                        point = it.commissionPoint,
-                                        id = it.id,
-                                        region = it.commissionRegion
-                                    )
-                                },
-                                totalHistoryList = (data.currentList + data.completedList).map {
-                                    HistoryItem(
-                                        product = it.commission,
-                                        category = it.category,
-                                        date = it.commissionDate,
-                                        point = it.commissionPoint,
-                                        id = it.id,
-                                        region = it.commissionRegion
-                                    )
-                                }
-                            )
-                        }
-                    }.onFailure {
-                        it.printStackTrace()
+        when (historyType) {
+            HistoryType.WORK -> {
+                historyUseCase.getCommission().onSuccess { data ->
+                    _uiState.update {
+                        HistoryUiState(
+                            stats = StatsType.Work.All(
+                                totalWork = data.completed,
+                                totalTime = data.countOrHour,
+                                totalPoint = data.point
+                            ) to StatsType.Work.Detail(
+                                mostRegion = data.mostRegion.koreanName,
+                                averageTime = data.countOrHour / data.completed,
+                                mostCategory = (data.currentList + data.completedList)
+                                    .map { it.category }
+                                    .groupingBy { it }
+                                    .eachCount()
+                                    .maxByOrNull { it.value }
+                                    ?.key ?: ""
+                            ),
+                            currentHistoryList = data.currentList.map {
+                                HistoryItem(
+                                    product = it.commission,
+                                    category = it.category,
+                                    date = it.commissionDate,
+                                    point = it.commissionPoint,
+                                    id = it.id,
+                                    region = it.commissionRegion
+                                )
+                            },
+                            totalHistoryList = (data.currentList + data.completedList).map {
+                                HistoryItem(
+                                    product = it.commission,
+                                    category = it.category,
+                                    date = it.commissionDate,
+                                    point = it.commissionPoint,
+                                    id = it.id,
+                                    region = it.commissionRegion
+                                )
+                            }
+                        )
                     }
+                }.onFailure {
+                    it.printStackTrace()
                 }
+            }
 
-                HistoryType.REQUEST -> {
-                    historyUseCase.getRequest().onSuccess { data ->
-                        _uiState.update {
-                            HistoryUiState(
-                                stats = StatsType.Request.All(
-                                    totalRequests = data.countOrHour,
-                                    totalPoint = data.point,
-                                    completedRequests = data.completed
-                                ) to StatsType.Request.Detail(
-                                    mostRegion = data.mostRegion.koreanName,
-                                    mostCategory = data.mostCategory.koreanName,
-                                    averagePoint = data.countOrHour / data.completed
-                                ),
-                                currentHistoryList = data.currentList.map {
-                                    HistoryItem(
-                                        product = it.commission,
-                                        category = it.category,
-                                        date = it.commissionDate,
-                                        point = it.commissionPoint,
-                                        id = it.id,
-                                        region = it.commissionRegion
-                                    )
-                                },
-                                totalHistoryList = (data.currentList + data.completedList).map {
-                                    HistoryItem(
-                                        product = it.commission,
-                                        category = it.category,
-                                        date = it.commissionDate,
-                                        point = it.commissionPoint,
-                                        id = it.id,
-                                        region = it.commissionRegion
-                                    )
-                                }
-                            )
-                        }
-                    }.onFailure {
-                        it.printStackTrace()
+            HistoryType.REQUEST -> {
+                historyUseCase.getRequest().onSuccess { data ->
+                    _uiState.update {
+                        HistoryUiState(
+                            stats = StatsType.Request.All(
+                                totalRequests = data.countOrHour,
+                                totalPoint = data.point,
+                                completedRequests = data.completed
+                            ) to StatsType.Request.Detail(
+                                mostRegion = data.mostRegion.koreanName,
+                                mostCategory = data.mostCategory,
+                                averagePoint = data.countOrHour / data.completed
+                            ),
+                            currentHistoryList = data.currentList.map {
+                                HistoryItem(
+                                    product = it.commission,
+                                    category = it.category,
+                                    date = it.commissionDate,
+                                    point = it.commissionPoint,
+                                    id = it.id,
+                                    region = it.commissionRegion
+                                )
+                            }.distinctBy { it.id },
+                            totalHistoryList = (data.currentList + data.completedList).map {
+                                HistoryItem(
+                                    product = it.commission,
+                                    category = it.category,
+                                    date = it.commissionDate,
+                                    point = it.commissionPoint,
+                                    id = it.id,
+                                    region = it.commissionRegion
+                                )
+                            }.distinctBy { it.id }
+                        )
                     }
+                }.onFailure {
+                    it.printStackTrace()
                 }
             }
         }
+    }
 }
